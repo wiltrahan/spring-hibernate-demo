c542b81d5d9d35b855599113274d71fddbe68379
wiltrahan <twilorip@gmail.com>
2018-04-13 16:26:39 -0400

    initial commit

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Coach theCoach = new TrackCoach();
+		
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
